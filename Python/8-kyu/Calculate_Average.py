bbeb9504ed4f698ca790e440bf5b38c66d179eff
OwlNet-C <45668467+OwlNet-C@users.noreply.github.com>
2022-02-03 16:35:23 +0000

    Create Calculate_Average.py

---
+++ Python/8-kyu/Calculate_Average.py
@@ -0,0 +1,13 @@
+'''
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+
+FUNDAMENTALSFUNCTIONAL PROGRAMMINGDECLARATIVE PROGRAMMING
+'''
+
+def find_average(numbers):
+    mean = sum(numbers) / (len(numbers))
+    return mean
+  
+  
