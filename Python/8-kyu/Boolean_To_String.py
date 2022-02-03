c14af1e69b7d6ea16bb5a0e5b603a608df1718b0
OwlNet-C <45668467+OwlNet-C@users.noreply.github.com>
2022-02-03 21:43:49 +0000

    Create Boolean_To_String.py

---
+++ Python/8-kyu/Boolean_To_String.py
@@ -0,0 +1,19 @@
+'''
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+'''
+
+def boolean_to_string(b):
+    word = str(b)
+    return(word)
+  
+  
+  
+ '''
+ Top rated soloution 
+ 
+ def boolean_to_string(b):
+    return str(b)
+    
+ '''
