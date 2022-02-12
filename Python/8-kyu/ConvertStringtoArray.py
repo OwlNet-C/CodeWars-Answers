773f5a63600cb6799f69fc5b63d9d9a4bf65ab5b
OwlNet-C <45668467+OwlNet-C@users.noreply.github.com>
2022-02-12 21:16:06 +0000

    Create ConvertStringtoArray.py

---
+++ Python/8-kyu/ConvertStringtoArray.py
@@ -0,0 +1,17 @@
+#https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/python
+'''
+Write a function to split a string and convert it into an array of words. For example:
+
+'''
+
+def string_to_array(s):
+    return s.split(" ")
+
+string_to_array("Robin Singh")
+
+'''
+Top answer:
+def string_to_array(s):
+    return s.split(' ')
+
+'''
