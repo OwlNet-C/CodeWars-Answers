3836da7cf534e687f7510f30afbcf1b7f58ab960
OwlNet-C <45668467+OwlNet-C@users.noreply.github.com>
2022-02-17 20:33:56 +0000

    Create AreaORPerimeter.py

---
+++ Python/8-kyu/AreaORPerimeter.py
@@ -0,0 +1,30 @@
+#https://www.codewars.com/kata/5ab6538b379d20ad880000ab/train/python
+
+'''
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+area_or_perimeter(6, 10) --> 32
+area_or_perimeter(3, 3) --> 9
+Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+'''
+
+
+def area_or_perimeter(l , w):
+    if l == w:
+        return (l*w)
+    else:
+        return (l+w+l+w) 
+
+    # return your answer
+
+area_or_perimeter(4, 4)
+
+
+
+'''
+Top rated answer:
+def area_or_perimeter(l, w):
+    return l * w if l == w else (l + w) * 2
+    
+'''
