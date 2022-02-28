09f50721696197ec0af946e4c68b41c8941a97a3
OwlNet-C <45668467+OwlNet-C@users.noreply.github.com>
2022-02-28 17:26:16 +0000

    Create ConvertNumtoStr.py

---
+++ Python/8-kyu/ConvertNumtoStr.py
@@ -0,0 +1,22 @@
+#https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/python
+'''
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+123 --> "123"
+'''
+def number_to_string(num):
+  return str(num)
+
+    
+
+
+
+number_to_string(67)
+
+'''
+Personal favourite:
+
+'''
