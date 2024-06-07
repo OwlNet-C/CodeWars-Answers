4fa1d7b63f59fd9e97c4c41b0a68004c9546df79
OwlNet-C <45668467+OwlNet-C@users.noreply.github.com>
2024-06-07 15:29:04 +0100

    Update Training on Fake Binary.sql

--- SQL/8-Kyu/Training on Fake Binary.sql
+++ SQL/8-Kyu/Training on Fake Binary.sql
@@ -4,7 +4,6 @@ Given a string of digits, you should replace any digit below 5 with '0' and any 
 Note: input will never be an empty string
 */
 
-
 ----------------
 --Answer
 
@@ -15,3 +14,12 @@ Note: input will never be an empty string
 select x,
 TRANSLATE(X,'1234567890','0000111110') as res
 from fakebin
+
+--Translate here is used to directly switch characters out for characters you intend 
+-- It is important to note the 2nd and 3rd input have to be of same length so that SQL recognises the like for like change
+-- if i had a 2nd input of 123 and 3rd in put of abcdef , 'def' dont have a corresponding translate option
+
+--Translate is case sensetive and mainly used for swapping out characters
+-- the REPLACE function would be the equivalent for words
+-- SELECT REPLACE(column_name, 'cat', 'dog') AS replaced_column 
+-- this exmaple will replace the word CAT for DOG in a column
