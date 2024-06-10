c38ea90b75e08445c589975c15bb20b017d2d2b0
OwlNet-C <45668467+OwlNet-C@users.noreply.github.com>
2024-06-10 16:10:33 +0100

    Create Distinct.sql

---
+++ SQL/8-Kyu/Distinct.sql
@@ -0,0 +1,7 @@
+================== Question
+
+For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+
+----------- Answer
+
+select distinct age from people
