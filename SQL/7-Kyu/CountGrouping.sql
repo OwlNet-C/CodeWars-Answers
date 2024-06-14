1224ec273bc8395863b3d5568e1a7d912e8dc71b
OwlNet-C <45668467+OwlNet-C@users.noreply.github.com>
2024-06-14 13:10:58 +0100

    Create CountGrouping.sql

---
+++ SQL/7-Kyu/CountGrouping.sql
@@ -0,0 +1,21 @@
+------------------ Question 
+/*Given a demographics table in the following format:
+
+** demographics table schema **
+
+id
+name
+birthday
+race
+you need to return a table that shows a count of each race represented, ordered by the count in descending order as:
+
+** output table schema **
+
+race
+count
+*/
+-------------------------------- Answer
+select distinct(race),count(race)
+from demographics
+group by race
+order by 2
